3998ed0ca83f46dbb8159a196225db3dace746ef
hubertdurnass <hubertdurnas17@gmail.com>
2025-01-09 21:40:03 +0100

    Update TaskManagerImplementation.cpp

--- TaskManagerImplementation.cpp
+++ TaskManagerImplementation.cpp
@@ -5,14 +5,24 @@
 
 namespace hd {
 
+/**
+ * @brief Constructor for TaskManager.
+ * @param fileName Name of the file to save/load tasks.
+ */
 TaskManager::TaskManager(const std::string& fileName) : fileName(fileName) {
     load();
 }
 
+/**
+ * @brief Destructor for TaskManager.
+ */
 TaskManager::~TaskManager() {
     save();
 }
 
+/**
+ * @brief Saves tasks to file.
+ */
 void TaskManager::save() {
     std::ofstream file(fileName);
     for (const auto& task : tasks) {
@@ -20,6 +30,9 @@ void TaskManager::save() {
     }
 }
 
+/**
+ * @brief Loads tasks from file.
+ */
 void TaskManager::load() {
     std::ifstream file(fileName);
     if (file.is_open()) {
@@ -33,34 +46,57 @@ void TaskManager::load() {
     }
 }
 
+/**
+ * @brief Adds a new task.
+ * @param description Description of the task.
+ */
 void TaskManager::addTask(const std::string& description) {
     tasks.emplace_back(description);
 }
 
+/**
+ * @brief Deletes a task by index.
+ * @param index Index of the task to delete.
+ */
 void TaskManager::deleteTask(int index) {
     if (index >= 0 && index < tasks.size()) {
         tasks.erase(tasks.begin() + index);
     }
 }
 
+/**
+ * @brief Edits a task description.
+ * @param index Index of the task to edit.
+ * @param newDescription New description of the task.
+ */
 void TaskManager::editTask(int index, const std::string& newDescription) {
     if (index >= 0 && index < tasks.size()) {
         tasks[index].description = newDescription;
     }
 }
 
+/**
+ * @brief Marks a task as done.
+ * @param index Index of the task to mark as done.
+ */
 void TaskManager::markAsDone(int index) {
     if (index >= 0 && index < tasks.size()) {
         tasks[index].done = true;
     }
 }
 
+/**
+ * @brief Displays all tasks.
+ */
 void TaskManager::displayTasks() const {
     for (int i = 0; i < tasks.size(); ++i) {
         std::cout << i + 1 << ". " << (tasks[i].done ? "[x] " : "[ ] ") << tasks[i].description << "\n";
     }
 }
 
+/**
+ * @brief Shows the menu for task management.
+ */
 void TaskManager::menu() {
     int choice;
     do {
@@ -113,11 +149,22 @@ void TaskManager::menu() {
     } while (choice != 6);
 }
 
+/**
+ * @brief Index operator for TaskManager.
+ * @param index Index of the task to access.
+ * @return Reference to the task.
+ */
 Task& TaskManager::operator[](int index) {
     return tasks[index];
 }
 
+/**
+ * @brief Const index operator for TaskManager.
+ * @param index Index of the task to access.
+ * @return Const reference to the task.
+ */
 const Task& TaskManager::operator[](int index) const {
     return tasks[index];
 }
-} //hd
+
+} // namespace hd
